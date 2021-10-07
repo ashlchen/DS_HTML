c151de41b03b791de5bea37db5c1a0929fec6eea
ashlchen <ashlchen@iu.edu>
2021-10-06 21:26:52 -0400

    10/6 class material

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,64 +1,60 @@
-const Offer = {
-    // const declaring an object. const is faster than var; it's constant, not changing; can skip all dynamic stuff
-    // or var 
-    data() { 
-        // pass function (returns an object) (make data lazy and save storage)
-      return {
-        "person": {},
-        "offer":[
-        {
-            "id": 1,
-            "name": "Ashley Chen",
-            "salary": 100000,
-            "bonus": 20000,
-            "company": "Google",
-            "offerDate": "2021-09-20"
-        },
-        {
-            "id": 2,
-            "name": "Ashley Chen",
-            "salary": 100000,
-            "bonus": 20000,
-            "company": "Google",
-            "offerDate": "2021-09-20"
-        }
-        ]
-    }
-    },
-    computed: {
-    prettyBirthday() {
-      return dayjs(this.person.dob.date).format('D MMM  YYYY')
-
-    }
-
-  },
-    methods: {
-    fetchuserData() {
-      // A
-    fetch('https://randomuser.me/api/') 
-    // fetch the url, a promise object
-    .then(response => response.json() )
-    // put the response (promise object) into json format (json object)
-    .then( (responseJson) => {
-      // another function to print in the function
-      console.log(responseJson);
-      // print in the console
-// C
-      this.person = responseJson.results[0];
-      // random user 1st the result store into person
-
-
-    })
-    .catch( (err) => {
-      // otherwise, return error
-      console.error(err);
-    })
-    // B
+const SomeApp = {
+  data() {
+    return {
+      students: [],
+      selectedStudent: null,
+      offers: []
     }
   },
-    created() {
-    this.fetchuserData();
-  }  
+  computed: {},
+  methods: {
+      prettyData(d) {
+          return dayjs(d)
+          .format('D MMM YYYY')
+      },
+      prettyDollar(n) {
+          const d = new Intl.NumberFormat("en-US").format(n);
+          return "$ " + d;
+      },
+      selectStudent(s) {
+          if (s == this.selectedStudent) {
+              return;
+          }
+          this.selectedStudent = s;
+          this.offers = [];
+          this.fetchOfferData(this.selectedStudent);
+      },
+      fetchStudentData() {
+          fetch('/api/student/')
+          .then( response => response.json() )
+          .then( (responseJson) => {
+              console.log(responseJson);
+              this.students = responseJson;
+          })
+          .catch( (err) => {
+              console.error(err);
+          })
+      },
+      fetchOfferData(s) {
+          console.log("Fetching offer data for ", s);
+          fetch('/api/offer/?student=' + s.id)
+          .then( response => response.json() )
+          .then( (responseJson) => {
+              console.log(responseJson);
+              this.offers = responseJson;
+          })
+          .catch( (err) => {
+              console.error(err);
+          })
+          .catch( (error) => {
+              console.error(error);
+          });
+      }
+  },
+  created() {
+      this.fetchStudentData();
+  }
+
 }
-  Vue.createApp(Offer).mount('#offerApp')
-//   mount = attach
+
+Vue.createApp(SomeApp).mount('#offerApp');
