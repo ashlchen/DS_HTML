38168794190e09afe7e2b8554a87d7eda492a9ff
ashlchen <ashlchen@iu.edu>
2021-10-29 12:52:49 -0400

    Add delete and update.php

---
+++ app/public/js/randomuser.js
@@ -0,0 +1,40 @@
+const Offer = {
+    // const declaring an object. const is faster than var; it's constant, not changing; can skip all dynamic stuff
+    // or var 
+    data() { 
+        // pass function (returns an object) (make data lazy and save storage)
+      return {
+        person: {},
+        
+    }
+    },
+    methods: {
+    fetchuserData() {
+      // A
+    fetch('https://randomuser.me/api/') 
+    // fetch the url, a promise object
+    .then(response => response.json() )
+    // put the response (promise object) into json format (json object)
+    .then( (responseJson) => {
+      // another function to print in the function
+      console.log(responseJson);
+      // print in the console
+// C
+      this.person = responseJson.results[0];
+      // random user 1st the result store into person
+
+
+    })
+    .catch( (err) => {
+      // otherwise, return error
+      console.error(err);
+    })
+    // B
+    }
+  },
+    created() {
+    this.fetchuserData();
+  }  
+}
+  Vue.createApp(Offer).mount('#offerApp')
+//   mount = attach
